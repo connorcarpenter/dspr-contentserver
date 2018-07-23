1f0574bb8dc6fe622fa3181d61013e637f6c26ed
connorcarpenter <connorcarpenter@gmail.com>
2018-07-23 14:22:17 -0600

    content now reloads if it's been modified, so i can keep it up

--- main.cpp
+++ main.cpp
@@ -1,34 +1,131 @@
 #include <uWS/uWS.h>
+#include <fstream>
 #include <iostream>
+#include <sstream>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+struct ContentFile
+{
+    std::stringstream* ss;
+    long modified;
+};
+
+std::map<std::string, ContentFile> fileMap;
+
+std::stringstream* getFileStream(std::string fileName);
+
+bool loadFile(std::string fileName);
 
 int main() {
     uWS::Hub h;
-
-    std::string noResponse = "no response";
-    std::string bffResponse = "ws://localhost:3170";
+    uint portToRun = 3170;
 
     h.onHttpRequest([&](uWS::HttpResponse *res, uWS::HttpRequest req, char *data, size_t length, size_t remainingBytes)
     {
-        if (req.headers->keyLength == 3 && strncmp(req.headers->key, "get", 3) == 0)
-        {
-            //request is a GET request
-            if (req.headers->valueLength == 17 && strncmp(req.headers->value, "/orchestrator/bff", 17) == 0)
+            if (strncmp(req.headers->value, "/orchestrator/", 14) == 0)
             {
-                res->end(bffResponse.data(), bffResponse.length());
+                //call orchestrator service
+                res->end("ws://www.deuspora.com:3172", 26);
+
+                std::string path = std::string(req.headers->value, req.headers->valueLength);
+                std::cout << "dspr-contentserver: Called through to orchestrator service: " << path << std::endl;
                 return;
             }
-        }
+            else
+            {
+                std::string fileName = std::string(req.headers->value, req.headers->valueLength);
+                std::stringstream* file = getFileStream(fileName);
 
-        res->end(noResponse.data(), noResponse.length());
+                if (file == nullptr) {
+                    res->end(nullptr, 0);
+                    return;
+                }
+
+                res->end(file->str().data(), file->str().length());
+
+                std::cout << "dspr-contentserver: Served file: " << fileName << std::endl;
+                return;
+            }
     });
 
     h.getDefaultGroup<uWS::SERVER>().startAutoPing(30000);
-    if (h.listen("localhost", 3170)) {
-        std::cout << "Listening to port 3170" << std::endl;
+    if (h.listen("localhost", portToRun)) {
+        std::cout << "dspr-contentserver: Listening to port " << portToRun << "" << std::endl;
     } else {
-        std::cerr << "Failed to listen to port" << std::endl;
+        std::cerr << "dspr-contentserver: Failed to listen to port " << portToRun << "" << std::endl;
         return -1;
     }
 
     h.run();
+}
+
+std::string filePathBase = "/home/connor/Work/fips-deploy/dspr-frontend/wasm-make-release";
+struct stat result;
+
+std::stringstream* getFileStream(std::string fileName)
+{
+    //check whether item exists
+    auto it = fileMap.find(fileName);
+    if(it != fileMap.end())
+    {
+        //item exists already in map
+
+        //let's find last modified time for content
+        std::string completeFilePath = filePathBase + fileName;
+
+        if(stat(completeFilePath.c_str(), &result) != 0) {
+            std::cerr << "dspr-contentserver: could not get modified time for: " << fileName << std::endl;
+            return nullptr;
+        }
+
+        long newTimeModified = result.st_mtime;
+
+        auto contentFile = fileMap.at(fileName);
+
+        if (newTimeModified == contentFile.modified)
+            return contentFile.ss;
+
+        std::cout << "dspr-contentserver: Reloading file into memory: " << fileName << std::endl;
+        fileMap.erase(fileName);
+        if(loadFile(fileName))
+            return fileMap.at(fileName).ss;
+        return nullptr;
+    }
+    else
+    {
+        //item does not exist in map
+        std::cout << "dspr-contentserver: Loading file into memory: " << fileName << std::endl;
+        if(loadFile(fileName))
+            return fileMap.at(fileName).ss;
+        return nullptr;
+    }
+}
+
+bool loadFile(std::string fileName)
+{
+    std::string completeFilePath = filePathBase + fileName;
+
+    if(stat(completeFilePath.c_str(), &result) != 0) {
+        std::cerr << "dspr-contentserver: could not get modified time for: " << fileName << std::endl;
+        return false;
+    }
+
+    auto mod_time = result.st_mtime;
+
+
+    std::stringstream* newFile = new std::stringstream();
+    (*newFile) << std::ifstream (completeFilePath).rdbuf();
+    if (!newFile->str().length()) {
+        std::cerr << "dspr-contentserver: Failed to load: " << fileName << std::endl;
+        return false;
+    }
+
+    ContentFile contentFile;
+    contentFile.ss = newFile;
+    contentFile.modified = mod_time;
+
+    fileMap.insert(std::pair<std::string, ContentFile>(fileName, contentFile));
+
+    return true;
 }
